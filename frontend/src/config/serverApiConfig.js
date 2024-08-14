c34577dec72a6d7b64839184ab35983e4aefc7e2
fescii <isfescii@gmail.com>
2024-08-14 12:41:55 +0300

    Fixed backend issue: Entry A04

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,11 +1,11 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:8888/api/';
+    : 'http://localhost:8000/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://localhost:8888/';
+    : 'http://localhost:8000/';
 
 export const WEBSITE_URL = import.meta.env.PROD
   ? 'http://cloud.BillkolosSoftapp.com/'
@@ -13,7 +13,7 @@ export const WEBSITE_URL = import.meta.env.PROD
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:8888/download/';
+    : 'http://localhost:8000/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
 export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
